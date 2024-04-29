a9b2c6886d0c9acb0c9d77a592cf45a917652ccb
RyanChua <ryanchua.23@ichat.sp.edu.sg>
2024-04-29 16:47:36 +0800

    update

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,27 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_more():
+    result = []
+    input_arr = [64, 34, 89, 70, 89, 69, 10, 35, 60, 57]
+
+    result = Lab3.bubble_sort(input_arr,1)
+
+    assert (result == 1)
+
+def test_bubble_zero():
+    result=[]
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr,1)
+
+    assert (result == 0)
+
+def test_bubble_noninteger():
+    result=[]
+    input_arr = [12.4, 16, 9.7]
+
+    result = Lab3.bubble_sort(input_arr,1)
+
+    assert (result == 2)
